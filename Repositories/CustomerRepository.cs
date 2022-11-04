a84ece2751ff5a2412a0a3bf452ea3aca9e40d1f
Stu Schwartz <stu.schwartz@live.com>
2022-11-04 18:33:35 -0400

    About to add DB Context

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -5,15 +5,23 @@ namespace Dynatron.Repositories
 {
     public class CustomerRepository : ICustomerRepository
     {
-        
-    // THIS LIST IS FOR DEV ONLY - MUST BE REPLACED BY DATABASE OBJECTS
-    public List<Customer> tempList = new List<Customer>{
+        // THIS LIST IS FOR DEV ONLY - MUST BE REPLACED BY DATABASE OBJECTS
+        public List<Customer> tempList = new List<Customer>{
         new Customer{ Id = 1, FirstName="Repo_Fred", LastName = "1", EmailAddress="1@1.com"},
         new Customer{ Id = 2, FirstName="Ed", LastName = "2", EmailAddress="2@2.com"},
         new Customer{ Id = 3, FirstName="Ted", LastName="3", EmailAddress="3@3.com"},
         new Customer{ Id = 4, FirstName = "Zed", LastName="4", EmailAddress = "4@4.com"}
     };
 
+        private readonly IConfiguration _configuration;
+        private readonly string dynatronConnectionString;
+
+        // Constructor
+        public CustomerRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            dynatronConnectionString = _configuration.GetConnectionString("Dynatron");
+        }
         public List<Customer> GetCustomers()
         {
             return tempList;
